a72e5542825ea7a2bbfbd7994a505f7d985c9cd9
Matei_Dana <marydanamat@gmail.com>
2025-07-27 15:10:54 +0300

    Initial commit/Project structure

--- obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpendSmart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d1317ee7c1884d64e1ef88abd8c7f666b6a3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpendSmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
