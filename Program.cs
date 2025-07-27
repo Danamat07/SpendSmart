07e1748274bfa999358ee9bda60494327bf39670
Matei_Dana <marydanamat@gmail.com>
2025-07-27 20:49:05 +0300

    setting up the database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using SpendSmart.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace SpendSmart
 {
     public class Program
@@ -8,6 +11,9 @@ namespace SpendSmart
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<SpendSmartContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+            );
 
             var app = builder.Build();
 
